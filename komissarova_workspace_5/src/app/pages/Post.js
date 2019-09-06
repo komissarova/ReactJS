94c01510ad706fab44189fdc448ec5b1ba6c4e7b
komissarova <elya_komissarova@mail.ru>
2019-09-06 10:50:46 +0300

    Добавила файл Post.js страницы Post нашего React-приложения

---
+++ komissarova_workspace_5/src/app/pages/Post.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+import PostProfile from '../components/Post';
+
+export default class Post extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      post: null
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.post ? <PostProfile {...this.state.post} /> : null}
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    axios.get(`http://jsonplaceholder.typicode.com/posts/${this.props.params.postId}`)
+      .then(response => {
+        const post = response.data;
+        this.setState({ post })
+      })
+  }
+}
